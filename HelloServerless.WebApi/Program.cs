620fba33cd066752e4155c2d39c41503dedb12bc
Serdar Kalaycı <serdarkalayci@Serdars-MacBook-Pro.local>
2020-02-20 17:16:41 +0300

    Add WebApi

--- HelloServerless.WebApi/Program.cs
+++ HelloServerless.WebApi/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace HelloServerless.WebApp
+namespace HelloServerless.WebApi
 {
     public class Program
     {
@@ -16,16 +16,11 @@ namespace HelloServerless.WebApp
             CreateHostBuilder(args).Build().Run();
         }
 
-public static IHostBuilder CreateHostBuilder(string[] args)
-{
-   string port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-   string url = String.Concat("http://localhost:", port);
-
-  return Host.CreateDefaultBuilder(args)
-      .ConfigureWebHostDefaults(webBuilder =>
-      {
-          webBuilder.UseStartup<Startup>().UseUrls(url);
-      });
-}
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
